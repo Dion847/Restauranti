6c89e3cfc6b93fcf93db2a0b98795f37ca2069f2
Dion847 <dion.valla02@gmail.com>
2024-02-03 20:32:30 +0100

    Header

---
+++ Header.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="Css/Style.css">
+<title>Flavor Haven Restaurant</title>
+<header>
+        <div class="headeri">
+            <div class="name"> 
+                <p>Flavor Haven Restaurant</p>
+            </div>
+        </div>
+        <ul>
+            <li><a style="text-decoration:none; color: black;" href="Home.php">Home</a></li>
+            <li><a style="text-decoration:none; color: black;" href="News.php">News</a></li>
+            <li><a style="text-decoration:none; color: black;" href="Menu.php">Menu</a></li>
+            <li><a style="text-decoration:none; color: black;" href="About.php">About Us</a></li>
+            <li><a style="text-decoration:none; color: black;" href="Contact Us.php">Contact Us</a></li>
+            <li><a style="text-decoration:none; color: black;" href="Login form.php">Log In</a></li>
+        </ul>
+</header>
