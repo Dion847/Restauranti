633221c486656cf9ac98920c87931dd74a9a52e9
drin-ai <driniisejdiu@gmail.com>
2024-02-03 20:46:55 +0100

    About

---
+++ About.php
@@ -0,0 +1,33 @@
+<?php include("header.php");?>
+    <link rel="stylesheet" href="Css/About.css">
+</head>
+<body>
+    <main>
+        <div class="bcgfoto">
+            <div class="abouttext">
+                <h1>About Us</h1>
+                <br>
+                <p>
+                    <b>Welcome to Flavor Haven,</b> where passion meets palate in every bite! Nestled in the heart of the bustling city, 
+                    our burger restaurant is a culinary haven for burger enthusiasts seeking an unforgettable dining experience.
+                </p>
+                <p>
+                    At Flavor Haven, we take pride in crafting mouthwatering burgers that transcend the ordinary, 
+                    using only the finest quality ingredients sourced from local farmers and producers.
+                </p>
+                <p>
+                    Our menu boasts a symphony of flavors, 
+                    from the classic American cheeseburger to innovative and exotic creations that cater to diverse tastes.
+                </p>
+                <p>
+                    The ambiance of our restaurant is warm and inviting, 
+                    providing the perfect backdrop for friends and family to come together and savor the delights of our delectable offerings.
+                </p>
+                <p> 
+                    Come join us and indulge in a culinary adventure that is bound to leave your taste buds dancing with delight!
+                </p>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
