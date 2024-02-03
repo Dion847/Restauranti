fc375afc3eef3d7e75a0d412d661973dd6b9cf7d
drin-ai <driniisejdiu@gmail.com>
2024-02-03 20:39:21 +0100

    Footer.php

---
+++ Footer.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="Style.css">
+<title>Flavor Haven Restaurant</title>
+<footer>
+        <div class="f">
+            <h2>Our Locations</h2>
+            <h2>Opening Hours</h2>
+            <h2>Contact</h2>
+        </div>
+        <div class="footermain">
+            <div class="footerleft">
+                <p>Prishtine, rr.Muharrem Fejza</p>
+                <p>Prishtine, rr.Mehmet Gradica</p>
+            </div>
+            <div class="footercenter">
+                <p>Monday-Friday: 08:00-00:00</p>
+                <p>Saturday-Sunday: 08:00-22:00</p>
+            </div>
+            <div class="footerright">
+                <p>+383 45 111 222</p>
+                <p>+383 44 111 222</p>
+            </div>
+        </div>
+</footer>
